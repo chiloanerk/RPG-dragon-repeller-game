134bf6783417f82f8f205fa5b1b6944071684460
chiloanerk <chiloanerk@gmail.com>
2024-03-04 20:05:06 +0200

    Complete

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ const locations = [
   {
     name: "kill monster",
     "button text": ["Go to town square", "Go to town square", "Go to town square"],
-    "button functions": [goTown, goTown, goTown],
+    "button functions": [goTown, goTown, easterEgg],
     text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
   },
   {
@@ -280,6 +280,9 @@ function pick(guess) {
     text.innerText += "Wrong! You lose 10 health!";
     health -= 10;
     healthText.innerText = health;
+    if (health <= 0) {
+      lose();
+    }
   }
 }
 
